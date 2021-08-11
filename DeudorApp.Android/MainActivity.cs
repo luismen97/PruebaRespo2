851d9df909e6cd4d99d34275ae517547805bd364
Luis Mendoza <luismen.m97@hotmail.com>
2021-08-11 17:42:20 -0500

    app v1

--- DeudorApp.Android/MainActivity.cs
+++ DeudorApp.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Xamarin.Essentials;
 
 namespace DeudorApp.Droid
 {
-    [Activity(Label = "DeudorApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "DeudorApp", Icon = "@mipmap/icon", Theme = "@style/Theme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
